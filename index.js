bb8c8515c69193050fa4641ec18447c9e8aabd27
topflyingdragon <topflyingdragon@126.com>
2017-08-01 18:09:12 +0800

    1

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ var port = new SerialPort('/dev/ttyS0', {
 });
 
 port.on('open', function() {
-    console.log("open");
+    console.log("port open");
+    start_test();
+});
+
+function start_test(){
+    const msg1 = 'set_config&TRANSMIT_TIMER_INTERVAL=5&ACK& ';
+    const msg2 = 'set_config&RECIVE_TIMER_INTERVAL=5&ACK& ';
+    
+    port.write(msg1, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written:' + msg1);
+    });
+    port.write(msg2, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written:' + msg2);
+    });
+
     setInterval(function(){
         console.log(port.read());
     },1000);
-});
+}
